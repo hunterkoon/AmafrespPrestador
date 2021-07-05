5b31226d577fef2c3b380ad7872233bcc1106900
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-07-05 06:46:20 -0300

    update

---
+++ src/Pages/Components/MenuPrestador.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MenuPrestador.css';
+const MenuPrestador = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>Conta</li>
+        <li>Usuários</li>
+        <li>Recadastramento</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MenuPrestador;
--- src/Pages/Conta.jsx
+++ src/Pages/Conta.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Conta.css';
+import Menu from '../Pages/Components/MenuPrestador';
 
 const Conta = () => {
   return (
     <div className="main-conta">
+      <div>
+        <Menu />
+      </div>
+      <div></div>
       <p> Esta é a pagina da conta</p>
     </div>
   );
