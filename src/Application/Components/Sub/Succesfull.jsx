aef066743e0f0e64caa47b32fa1a582906b12785
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-09-17 10:00:58 -0300

    login validate

--- src/Application/Components/Sub/Succesfull.jsx
+++ src/Application/Components/Sub/Succesfull.jsx
@@ -3,22 +3,21 @@ import Title from "./Title";
 import Titledecorated from "./Titledecorated";
 import "./Succesfull.css";
 const Succesfull = ({ ...props }) => {
-
-
   return (
     <>
-      {props.alert ? <div className="main-modal"  >
-        <div className="div-main-successfull">
-        <div className="div-titles-successfull">
-            <Titledecorated text="Sucesso!!!" />
-            <Title text={props.text} />
-          </div>
-          <div className="div-close-modal" onClick={props.onClick}>
-            <span>x</span>
+      {props.alert ? (
+        <div className="main-modal">
+          <div className="div-main-successfull">
+            <div className="div-titles-successfull">
+              <Titledecorated text="Sucesso!!!" />
+              <Title text={props.text} />
+            </div>
+            <div className="div-close-modal" onClick={props.onClick}>
+              <span>x</span>
+            </div>
           </div>
         </div>
-      </div>
-        : null}
+      ) : null}
     </>
   );
 };
