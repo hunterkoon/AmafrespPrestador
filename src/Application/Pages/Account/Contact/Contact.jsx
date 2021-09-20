49e51afd2ae0f30a9f44739cc06b0f8a857c3521
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-09-20 15:32:50 -0300

    google maps implement

--- src/Application/Pages/Account/Contact/Contact.jsx
+++ src/Application/Pages/Account/Contact/Contact.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import Titledecorated from '../../../Components/Sub/Titledecorated';
-import Maintenance from '../../../Components/Main/Maintenance';
-import './Contact.css';
+import React from "react";
+import Titledecorated from "../../../Components/Sub/Titledecorated";
+import GoogleMaps from "google-map-react";
+import "./Contact.css";
 
 const FaleConosco = () => {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
   return (
-    <>
-      <div className="div-main-faleConosco pageView">
-        <div className="div-title-pages">
-          <Titledecorated text="Fale Conosco" />
-        </div>
-        <Maintenance />
+    <div className="div-main-faleConosco pageView">
+      <div className="div-title-pages">
+        <Titledecorated text="Fale Conosco" />
       </div>
-    </>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMaps
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        </GoogleMaps>
+      </div>
+    </div>
   );
 };
 
