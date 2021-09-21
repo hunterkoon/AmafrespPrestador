9c40d522596708595c134d3a9503cec135b98e61
GABRIEL CAMPOS <g.p.campos@uni9.edu.br>
2021-09-21 09:47:34 -0300

    contact page

--- src/Application/Pages/Account/Contact/Contact.jsx
+++ src/Application/Pages/Account/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Titledecorated from "../../../Components/Sub/Titledecorated";
 import GoogleMaps from "google-map-react";
-import ImgPin from '../../../../Assets/Pin.gif'
+import ImgPin from "../../../../Assets/Pin.gif";
 import "./Contact.css";
 
 const FaleConosco = () => {
@@ -12,30 +12,56 @@ const FaleConosco = () => {
     },
     zoom: 18,
   };
-   const AnyReactComponent = ({ img ,   }) =>   
-   <div className='maps'>
-     <img src={img} alt="pin map" />
-    <label>Av. Brigadeiro Luís Antônio, 4843 - Jardim Paulista, São Paulo - SP, 01401-002 <a style={{textDecoration:'underline'}}href={'https://g.page/Afresp?share'} target='_blank'  rel="noreferrer" >Ver no Maps</a> </label>
-  </div>;
+  const AnyReactComponent = ({ img }) => (
+    <div className="maps">
+      <img src={img} alt="pin map" />
 
-
+      <label>
+        <h1>Atendimento AMAFRESP</h1>
+        Av. Brigadeiro Luís Antônio, 4843 – 1 º Andar - Horário: das 8h às 17h
+        <a
+          style={{ textDecoration: "underline" }}
+          href={"https://g.page/Afresp?share"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ver no Maps
+        </a>
+      </label>
+    </div>
+  );
 
   return (
     <div className="div-main-faleConosco pageView">
       <div className="div-title-pages">
         <Titledecorated text="Fale Conosco" />
       </div>
-      <div style={{ height: "60vh", width: "100%" }}>
+      <div style={{ height: "60vh", width: "95%", margin: "0 auto" }}>
         <GoogleMaps
           bootstrapURLKeys={{ key: "AIzaSyChYgkieCv8PxYNuIamnInUGWgQhuZp5FI" }}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         >
-       <AnyReactComponent
-          lat={defaultProps.center.lat}
-          lng={defaultProps.center.lng}
-          img={ImgPin}/>
+          <AnyReactComponent
+            lat={defaultProps.center.lat}
+            lng={defaultProps.center.lng}
+            img={ImgPin}
+          />
         </GoogleMaps>
+      </div>
+
+      <div className="div-sub-contact">
+        <h1>Contato</h1>
+        <div>
+          <h3> Email:</h3>
+          <h4> amafresp@afresp.org.br </h4>
+          <h3> Local: </h3>
+          <h4>
+            Av. Brigadeiro Luís Antônio, 4843 – 1 º Andar Horário: das 8h às
+            17h.
+          </h4>
+          <h3>Central de Atendimento </h3> <h4> Tel.: (11) 4020-8721</h4>
+        </div>
       </div>
     </div>
   );
