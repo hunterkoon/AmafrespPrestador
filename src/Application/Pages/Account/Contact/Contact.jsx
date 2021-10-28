28dcdc1b9a7d180d4aff1d62ca1ee6a4cab0e9cd
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-10-28 17:43:23 -0300

    git en changed

--- src/Application/Pages/Account/Contact/Contact.jsx
+++ src/Application/Pages/Account/Contact/Contact.jsx
@@ -8,7 +8,7 @@ import Title from "../../../Components/Sub/Title";
 // import "mapbox-gl/dist/mapbox-gl.css";
 // import mapboxgl from "mapbox-gl";
 
-const FaleConosco = () => {
+const Contact = () => {
   const [viewport, setViewport] = React.useState({
     latitude: -23.583,
     longitude: -46.668,
@@ -16,6 +16,8 @@ const FaleConosco = () => {
     height: "100%",
     zoom: 18.32,
   });
+
+  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
 
   return (
     <div className="div-main-faleConosco pageView">
@@ -26,7 +28,7 @@ const FaleConosco = () => {
       <div className="map-open">
         <ReactMapGl
           {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           mapStyle="mapbox://styles/hunterkoon/ckvb0sybk8vx914mfs99ktqvx"
           onViewportChange={(viewport) => {
             setViewport(viewport);
@@ -64,60 +66,60 @@ const FaleConosco = () => {
         <h1>Telefones Úteis</h1>
 
         <ul className="tel-list">
-          
+
           <section>
-          <h3>Diretoria e Gerência da Amafresp</h3>
-          <li>Tel.: (11) 3886-8881</li>
+            <h3>Diretoria e Gerência da Amafresp</h3>
+            <li>Tel.: (11) 3886-8881</li>
           </section>
 
           <section>
-          <h3>Central de Atendimento</h3>
-          <li>Tel.: (11) 4020-8721</li>
+            <h3>Central de Atendimento</h3>
+            <li>Tel.: (11) 4020-8721</li>
           </section>
 
           <section>
-          <h3>Combate Covid (24h)</h3>
-          <li>Tel.: (11) 97503-8958 / (11) 97502-5941</li>
+            <h3>Combate Covid (24h)</h3>
+            <li>Tel.: (11) 97503-8958 / (11) 97502-5941</li>
           </section>
 
           <section>
-          <h3>Análise de Contas Médicas</h3>
-          <li>Tel.: (11) 3886-8885</li>
+            <h3>Análise de Contas Médicas</h3>
+            <li>Tel.: (11) 3886-8885</li>
           </section>
 
           <section>
-          <h3>Auditoria Médica</h3>
-          <li>Tel.: (11) 3886-8894 / (11) 3886-8845</li>
+            <h3>Auditoria Médica</h3>
+            <li>Tel.: (11) 3886-8894 / (11) 3886-8845</li>
           </section>
 
           <section>
-          <h3>Compras Médicas</h3>
-          <li>Tel.: (11)  4020-8721</li>
+            <h3>Compras Médicas</h3>
+            <li>Tel.: (11)  4020-8721</li>
           </section>
 
           <section>
-          <h3>Credenciamento</h3>
-          <li>Tel.: (11) 3886-8828</li>
+            <h3>Credenciamento</h3>
+            <li>Tel.: (11) 3886-8828</li>
           </section>
 
           <section>
-          <h3>Cobrança</h3>
-          <li>Tel.: (11) 4020-8721</li>
+            <h3>Cobrança</h3>
+            <li>Tel.: (11) 4020-8721</li>
           </section>
 
           <section>
-          <h3>Prevenção/Home Care</h3>
-          <li>Tel.: (11) 3886-8846 / (11) 3886-8856</li>
+            <h3>Prevenção/Home Care</h3>
+            <li>Tel.: (11) 3886-8846 / (11) 3886-8856</li>
           </section>
 
           <section>
-          <h3>Protocolo de Contas Médicas</h3>
-          <li>Tel.: (11) 3886-8928</li>
+            <h3>Protocolo de Contas Médicas</h3>
+            <li>Tel.: (11) 3886-8928</li>
           </section>
 
           <section>
-          <h3>Relacionamento com o associado</h3>
-          <li>Tel.: (11) 3886-8881 / (11) 3886-8813 <br/> (11) 3886-8878 / (11) 3886-8865 <br/> (11) 3886-8875</li>
+            <h3>Relacionamento com o associado</h3>
+            <li>Tel.: (11) 3886-8881 / (11) 3886-8813 <br /> (11) 3886-8878 / (11) 3886-8865 <br /> (11) 3886-8875</li>
           </section>
 
         </ul>
@@ -126,4 +128,4 @@ const FaleConosco = () => {
   );
 };
 
-export default FaleConosco;
+export default Contact;
