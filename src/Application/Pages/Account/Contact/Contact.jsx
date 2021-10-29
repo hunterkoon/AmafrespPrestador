adc02d21f891e2f5449e6c938b88468d75f0dd38
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-10-28 22:51:42 -0300

    contact

--- src/Application/Pages/Account/Contact/Contact.jsx
+++ src/Application/Pages/Account/Contact/Contact.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import Titledecorated from "../../../Components/Sub/Titledecorated";
 import ImgPin from "../../../../Assets/Pin.gif";
 import ReactMapGl, { Marker, Popup } from "react-map-gl";
-import "./Contact.css";
 import Title from "../../../Components/Sub/Title";
-// import GoogleMaps from "google-map-react";
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import mapboxgl from "mapbox-gl";
+import "./Contact.css";
+
 
 const Contact = () => {
   const [viewport, setViewport] = React.useState({
     latitude: -23.583,
     longitude: -46.668,
-    width: "80%",
-    height: "100%",
+    width: "100vh",
+    height: "100vw",
     zoom: 18.32,
   });
 
-  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
 
   return (
     <div className="div-main-faleConosco pageView">
@@ -69,61 +66,66 @@ const Contact = () => {
 
           <section>
             <h3>Diretoria e Gerência da Amafresp</h3>
-            <li>Tel.: (11) 3886-8881</li>
+            <li>(11) 3886-8881</li>
           </section>
 
           <section>
             <h3>Central de Atendimento</h3>
-            <li>Tel.: (11) 4020-8721</li>
+            <li> (11) 4020-8721</li>
           </section>
 
           <section>
             <h3>Combate Covid (24h)</h3>
-            <li>Tel.: (11) 97503-8958 / (11) 97502-5941</li>
+            <li> (11) 97503-8958 | (11) 97502-5941</li>
           </section>
 
           <section>
             <h3>Análise de Contas Médicas</h3>
-            <li>Tel.: (11) 3886-8885</li>
+            <li> (11) 3886-8885</li>
           </section>
 
           <section>
             <h3>Auditoria Médica</h3>
-            <li>Tel.: (11) 3886-8894 / (11) 3886-8845</li>
+            <li> (11) 3886-8894 | (11) 3886-8845</li>
           </section>
 
           <section>
             <h3>Compras Médicas</h3>
-            <li>Tel.: (11)  4020-8721</li>
+            <li> (11) 4020-8721</li>
           </section>
 
           <section>
             <h3>Credenciamento</h3>
-            <li>Tel.: (11) 3886-8828</li>
+            <li> (11) 3886-8828</li>
           </section>
 
           <section>
             <h3>Cobrança</h3>
-            <li>Tel.: (11) 4020-8721</li>
+            <li> (11) 4020-8721</li>
           </section>
 
           <section>
             <h3>Prevenção/Home Care</h3>
-            <li>Tel.: (11) 3886-8846 / (11) 3886-8856</li>
+            <li> (11) 3886-8846 | (11) 3886-8856</li>
           </section>
 
           <section>
             <h3>Protocolo de Contas Médicas</h3>
-            <li>Tel.: (11) 3886-8928</li>
+            <li> (11) 3886-8928</li>
           </section>
 
           <section>
             <h3>Relacionamento com o associado</h3>
-            <li>Tel.: (11) 3886-8881 / (11) 3886-8813 <br /> (11) 3886-8878 / (11) 3886-8865 <br /> (11) 3886-8875</li>
+            <li> (11) 3886-8881 | (11) 3886-8813 <br />
+             (11) 3886-8878 | (11) 3886-8865
+              (11) 3886-8875</li>
           </section>
-
         </ul>
       </div>
+      <div className="div-sub-ans">
+        <img src={""} alt={""} />
+      </div>
+
     </div>
   );
 };
