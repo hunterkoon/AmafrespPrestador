6db183f965eafab12a61a0107ad6d20621befb25
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-11-03 13:50:34 -0300

    webconfig

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,14 +6,13 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
     ],
-
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['my-custom-babel-preset'],
-        ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js']
-      }
-    }
-
   },
+
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['my-custom-babel-preset'],
+      ignore: ['./node_modules/mapbox-gl/dist/mapbox-gl.js']
+    }
+  }
 };
