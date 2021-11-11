7020d44c3678cdc944064e8a347e0abb5e44896f
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-11-11 16:00:07 -0300

    Maps

--- src/Application/Pages/Account/Contact/MapContact.jsx
+++ src/Application/Pages/Account/Contact/MapContact.jsx
@@ -5,13 +5,13 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 const MapContact = () => {
   const position = [-23.583140, -46.667780]
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={position} zoom={18} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position}>
-        <Popup> A pretty CSS3 popup. <br /> Easily customizable. </Popup>
+        <Popup>  Av. Brigadeiro Luís Antônio, 4843 – 1 º Andar Horário: das 8h às 17h </Popup>
       </Marker>
     </MapContainer>
 
