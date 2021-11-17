67a54f65ade0a865b2fa7e92ae60ea5b03460495
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-11-17 20:45:24 -0300

    payment

--- src/Application/Pages/Account/Functionalities/Payments.jsx
+++ src/Application/Pages/Account/Functionalities/Payments.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import "./Payments.css";
 import Titledecorated from '../../../Components/Sub/Titledecorated';
-import { GlobalContext } from '../../Main/GlobalContext';
-import LoadPage from '../../../Components/Sub/Load';
 import PaymentsIframe from './Subs/PaymentsIframe';
+import "./Payments.css";
 
 const Payments = () => {
-  const [loads, setLoads] = React.useState(true);
-  const [payments, setPayments] = React.useState(null);
-
-  React.useEffect(() => {
-    setPayments(<PaymentsIframe />)
-    const setTime = () => {
-      setTimeout(() => setLoads(false), 1000)
-    }
-    setTime()
-  }, [loads])
 
   return (
     <div className="div-main-payments pageView">
