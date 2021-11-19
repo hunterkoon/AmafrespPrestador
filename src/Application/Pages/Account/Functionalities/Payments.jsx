1985f1de9bb5ad43068608d6325c75f982304883
gabrielcampos <gabrielcampos@AFRHOME02VM>
2021-11-19 15:27:49 -0300

    Payments Load

--- src/Application/Pages/Account/Functionalities/Payments.jsx
+++ src/Application/Pages/Account/Functionalities/Payments.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
 import Load from '../../../Components/Sub/Load'
-import Iframe from 'react-iframe-click';
 import Titledecorated from '../../../Components/Sub/Titledecorated';
 import "./Payments.css";
 
 const Payments = () => {
-
   const [loadingPage, setLoadingPage] = React.useState(true)
-
-  const handleLoading = (e) => {
-    if (e) {
-      setLoadingPage(!loadingPage)
-      setInterval(() => {
-        setLoadingPage(false)
-      }, 3000);
-    }
-  }
-
   return (
     <div className="div-main-payments pageView">
       <div className="div-title-pages">
         <Titledecorated text="Demonstrativos de Pagamento" />
       </div>
-      <Iframe
+      <iframe
+        sandbox
+        allow
+        allowfullscreen="false"
         className="iframe-function"
         frameBorder="0"
         src="http://localhost:2630/Contratados/cPagamento.aspx"
         loading="eager"
-        onInferredClick={() => setLoadingPage(true)}
-        onLoad={(e) => handleLoading(e)}
+        onLoad={() => setLoadingPage(false)}
       >
-      </Iframe>
+      </iframe>
       <Load load={loadingPage} />
     </div >
   )
